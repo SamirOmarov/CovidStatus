9388981221c00fa24ddb39406bf63dddb91370ea
Samir Omarov <samir.omarov@gmail.com>
2020-03-12 18:16:20 +0300

    add Footer component

--- src/layouts/components/TheFooter.vue
+++ src/layouts/components/TheFooter.vue
@@ -2,18 +2,15 @@
     File Name: TheFooter.vue
     Description: Footer component
     Component Name: TheFooter
-    ----------------------------------------------------------------------------------------
-    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-      Author: Pixinvent
-    Author URL: http://www.themeforest.net/user/pixinvent
+
 ========================================================================================== -->
 
 
 <template functional>
     <footer class="the-footer flex-wrap justify-between" :class="classes">
-        <span>COPYRIGHT &copy; {{ new Date().getFullYear() }} <a href="https://1.envato.market/vuexy_admin" target="_blank" rel="nofollow">Pixinvent</a>, All rights Reserved</span>
+        <span>&copy; {{ new Date().getFullYear() }} CovidSatus. </span>
         <span class="md:flex hidden items-center">
-            <span>Hand-crafted & Made with</span>
+            <span>Stay Safe</span>
             <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
         </span>
     </footer>
