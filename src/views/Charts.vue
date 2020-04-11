afe2203073b0f2d9bde678f866b51aaff19cfb0f
Seymur Omarov <omarov.seymur@outlook.com>
2020-04-11 23:05:34 +0400

    add new graphics about confirmed and death by population

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -13,9 +13,9 @@
       >
         <vs-select-item :key="index" :value="item.value" :text="item.text" v-for="(item,index) in countries"/>
       </vs-select>
-<!--      <vs-checkbox style="margin-left: 5px;margin-bottom: 10px" v-model="predict" v-on:change="prediction">-->
-<!--        {{$t('predict')}}-->
-<!--      </vs-checkbox>-->
+      <!--      <vs-checkbox style="margin-left: 5px;margin-bottom: 10px" v-model="predict" v-on:change="prediction">-->
+      <!--        {{$t('predict')}}-->
+      <!--      </vs-checkbox>-->
     </div>
     <div class="vx-row">
       <div class="md:w-1/2 w-full mb-base vx-col">
@@ -28,27 +28,39 @@
           <canvas id="confirmedChartByDay"></canvas>
         </vx-card>
       </div>
+
+      <div class="md:w-1/2 w-full mb-base vx-col">
+        <vx-card :title="$t('TotalInfectedByPopulationConfirmed')">
+          <canvas id="confirmedChartByPopulationConfirmed"></canvas>
+        </vx-card>
+      </div>
+      <div class="md:w-1/2 w-full mb-base vx-col">
+        <vx-card :title="$t('TotalDeathByPopulation')">
+          <canvas id="DeathChartByPopulation"></canvas>
+        </vx-card>
+      </div>
+
       <div class="md:w-1/2 w-full mb-base vx-col">
         <vx-card :title="$t('Symptoms')">
-          <symptoms />
+          <symptoms/>
         </vx-card>
       </div>
 
       <div class="md:w-1/2 w-full mb-base vx-col">
         <vx-card :title="$t('WorldWidePercentage')">
-          <percentage />
+          <percentage/>
         </vx-card>
       </div>
 
       <div class="md:w-1/2 w-full mb-base vx-col">
         <vx-card :title="$t('Preconditions')">
-          <risk />
+          <risk/>
         </vx-card>
       </div>
 
       <div class="md:w-1/2 w-full mb-base vx-col">
         <vx-card :title="$t('RiskGroup')">
-          <illness />
+          <illness/>
         </vx-card>
       </div>
 
@@ -140,19 +152,19 @@
         let response = await axios.get("https://api.covidstatus.com/cases/" + this.country.code.toLowerCase(), {})
         let from = Date.parse("2020-03-05");
 
+        let dates = []
         let countryData = {
-          dates: [],
           confirmed: [],
+          death: []
         }
         let countryDataByDay = {
-          dates: [],
           confirmed: [],
         }
         response.data.timeline.forEach(element => {
           if (from < Date.parse(element.date)) {
-            countryData.dates.push(element.date)
-            countryDataByDay.dates.push(element.date)
+            dates.push(element.date)
             countryData.confirmed.push(element.total_cases)
+            countryData.death.push(element.total_deaths)
             countryDataByDay.confirmed.push(element.new_daily_cases)
           }
         });
@@ -165,7 +177,7 @@
             diff = response.data.new_confirmed / countryDataByDay.confirmed[countryDataByDay.confirmed.length - 1]
           }
           for (let i = 1; i <= 7; i++) {
-            let date = countryData.dates[countryData.dates.length - 1]
+            let date = dates[dates.length - 1]
             let someDate = new Date(date);
             let numberOfDaysToAdd = 1;
             someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
@@ -173,16 +185,42 @@
             let mm = someDate.getMonth() + 1;
             let y = someDate.getFullYear();
             let someFormattedDate = y + '-' + mm + '-' + dd;
-            countryData.dates.push(someFormattedDate)
-            countryDataByDay.dates.push(someFormattedDate)
+            dates.push(someFormattedDate)
             countryDataByDay.confirmed.push(Math.round(countryDataByDay.confirmed[countryDataByDay.confirmed.length - 1] * diff))
             countryData.confirmed.push(Math.round(countryData.confirmed[countryData.confirmed.length - 1] + countryDataByDay.confirmed[countryDataByDay.confirmed.length - 1]))
           }
         }
+        let countryDataByPopulation = {
+          confirmed: [],
+          death: []
+        }
+        for (let i = 0; i < countryData.confirmed.length; i++) {
+          let confirmed = countryData.confirmed[i]
+          let death = countryData.death[i]
+          countryDataByPopulation.confirmed.push((confirmed * 100 / response.data.population).toFixed(5))
+          countryDataByPopulation.death.push((death * 100 / response.data.population).toFixed(5))
+        }
+
         let datasets = [{
           fill: false,
           label: this.country.code,
           data: countryData.confirmed,
+          borderColor: '#fe8b36',
+          backgroundColor: '#fe8b36',
+          display: true
+        }]
+        let datasetsByPopulaionConfirmed = [{
+          fill: false,
+          label: this.country.code,
+          data: countryDataByPopulation.confirmed,
+          borderColor: '#fe8b36',
+          backgroundColor: '#fe8b36',
+          display: true
+        }]
+        let datasetsByPopulaionDeath = [{
+          fill: false,
+          label: this.country.code,
+          data: countryDataByPopulation.death,
           borderColor: '#fe8b36',
           backgroundColor: '#fe8b36',
           display: true
@@ -200,9 +238,15 @@
         for (const property in this.selectedCountries) {
           countryData[property] = []
           countryDataByDay[property] = []
+          countryDataByPopulation[property + 'Confirmed'] = []
+          countryDataByPopulation[property + 'Death'] = []
           this.selectedCountries[property].timeline.forEach(element => {
             if (from < Date.parse(element.date)) {
               countryData[property].push(element.total_cases)
+              let percentageByPopulationConfirmed = (parseFloat(element.total_cases) * 100 / parseFloat(this.selectedCountries[property].population)).toFixed(5)
+              let percentageByPopulationDeath = (parseFloat(element.total_deaths) * 100 / parseFloat(this.selectedCountries[property].population)).toFixed(5)
+              countryDataByPopulation[property + 'Confirmed'].push(percentageByPopulationConfirmed)
+              countryDataByPopulation[property + 'Death'].push(percentageByPopulationDeath)
               countryDataByDay[property].push(element.new_daily_cases)
             }
           });
@@ -229,6 +273,22 @@
             backgroundColor: colors[colorsTurn],
             display: true
           })
+          datasetsByPopulaionConfirmed.push({
+            fill: false,
+            label: property,
+            data: countryDataByPopulation[property + 'Confirmed'],
+            borderColor: colors[colorsTurn],
+            backgroundColor: colors[colorsTurn],
+            display: true
+          })
+          datasetsByPopulaionDeath.push({
+            fill: false,
+            label: property,
+            data: countryDataByPopulation[property + 'Death'],
+            borderColor: colors[colorsTurn],
+            backgroundColor: colors[colorsTurn],
+            display: true
+          })
           datasetsByDay.push({
             fill: false,
             label: property,
@@ -242,11 +302,19 @@
 
 
         let dataDate = {
-          labels: countryData.dates,
+          labels: dates,
           datasets: datasets,
         }
+        let dataDateByPopulationConfirmed = {
+          labels: dates,
+          datasets: datasetsByPopulaionConfirmed,
+        }
+        let dataDateByPopulationDeath = {
+          labels: dates,
+          datasets: datasetsByPopulaionDeath,
+        }
         let dataDateByDay = {
-          labels: countryDataByDay.dates,
+          labels: dates,
           datasets: datasetsByDay,
         }
         let options = {
@@ -263,10 +331,6 @@
                 beginAtZero: true,
               },
               display: true,
-              // scaleLabel: {
-              //   display: true,
-              //   labelString: this.$t('TotalInfected'),
-              // }
             }]
           }
         }
@@ -281,8 +345,21 @@
           data: dataDateByDay,
           options: options
         };
+        let chartByPopulationConfirmed = {
+          type: 'line',
+          data: dataDateByPopulationConfirmed,
+          options: options
+        };
+        let chartByPopulationDeath = {
+          type: 'line',
+          data: dataDateByPopulationDeath,
+          options: options
+        };
         this.createChart('confirmedChart', chart);
         this.createChart('confirmedChartByDay', chartByDay);
+        this.createChart('confirmedChartByPopulationConfirmed', chartByPopulationConfirmed);
+        this.createChart('DeathChartByPopulation', chartByPopulationDeath);
+
 
       }
     },
